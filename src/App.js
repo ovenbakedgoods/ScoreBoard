7d4dc1f8fd0db3d2dfe6059cb0cfb6f3364294bd
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-07 16:11:18 -0500

    Breaking app into modular components

--- src/App.js
+++ src/App.js
@@ -1,77 +1,13 @@
 
 import React from "react"
 import './App.css';
-
-const Header = (props) => {
-  return (
-    <header>
-      <h1>{ props.title }</h1>
-      <span className="stats">Players: { props.totalPlayers }</span>
-    </header>
-  );
-}
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
-
-      <Counter />
-    </div>
-  );
-}
-
-class Counter extends React.Component {
-  state = {
-    score: 0
-  };
-
-  incrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score + 1
-    }));
-  }
-
-  decrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score - 1
-    }));
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
-}
+import Header from "./Header"
+import Player from "./Player";
+import players from "./players"
 
 class App extends React.Component {
   state = {
-    players: [
-      {
-        name: "Jabari",
-        id: 1
-      },
-      {
-        name: "Terri",
-        id: 2
-      },
-      {
-        name: "Ashley",
-        id: 3
-      },
-      {
-        name: "Parlan",
-        id: 4
-      }
-    ]
+    players
   };
 
   handleRemovePlayer = (id) => {
